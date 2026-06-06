c6d373a482307bdc04571355dfd41099ddc13a1d
fredefussing <fredefussing@gmail.com>
2026-06-06 10:40:58 +0000

    Fix Firebase auth for Forma Estates

--- firebase.js
+++ firebase.js
@@ -1,5 +1,10 @@
-const { initializeApp } = require('firebase/app');
-const { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } = require('firebase/auth');
+const { initializeApp } = require("firebase/app");
+const {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} = require("firebase/auth");
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY || "",
@@ -7,15 +12,15 @@ const firebaseConfig = {
   projectId: "nordic-homebuilding",
   storageBucket: "nordic-homebuilding.firebasestorage.app",
   messagingSenderId: "1086068815265",
-  appId: "1:1086068815265:web:1fd2452f4946d97c5f47e0"
+  appId: "1:1086068815265:web:1fd2452f4946d97c5f47e0",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-module.exports = { 
-  auth, 
-  createUserWithEmailAndPassword, 
+module.exports = {
+  auth,
+  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  signOut 
+  signOut,
 };
